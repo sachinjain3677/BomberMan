03bc8ce3c431d450c344db8be1e87be5587695d0
Sachin Jain <sachinjain3677@gmail.com>
2017-12-01 16:51:34 +0530

    Fixed errors of bomb prefab

---
+++ Assets/My Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//rename to game Controller
+public class GameController : MonoBehaviour {
+
+	Vector3 bombSpawn;
+	float tempTime;
+	
+	public GameObject bomb;
+	public float bombWaitTime;
+	public GameObject player;
+	
+	// Use this for initialization
+	void Start(){
+		tempTime = bombWaitTime;
+	}
+	
+	void FixedUpdate(){
+		bombSpawn = player.transform.position;
+		bombSpawn += Vector3.up * 0.35f;
+		
+		if(Input.GetKeyDown(KeyCode.F) && Time.time > tempTime){
+			//Debug.Log(Time.time);
+			tempTime += bombWaitTime;
+			GameObject spawnedBomb = (GameObject)Instantiate(bomb, bombSpawn, player.transform.rotation);
+			
+			Destroy(spawnedBomb,3);
+		}
+
+	}
+}
--- Assets/My Scripts/PlayerController.cs
+++ Assets/My Scripts/PlayerController.cs
@@ -6,23 +6,21 @@ public class PlayerController : MonoBehaviour {
 
 	float x;
 	float z;
-	Vector3 bombSpawn;
-	float tempTime;
 	
 	public float speed;
-	public GameObject bomb;
-	public float bombWaitTime;
 	
-	void Start(){
-		tempTime = bombWaitTime;
-	}
+	
 
 	// Update is called once per frame
 	void Update () {
 		x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
 		z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
+		movement(x,z);
+				
+	}
 
+	void movement(float x, float z){
 		if(x>0){
 
 			transform.eulerAngles = new Vector3(0,0,0);	
@@ -47,21 +45,9 @@ public class PlayerController : MonoBehaviour {
 				transform.Translate(-z,0,0);
 			}
 		}
-		
-	}
-
-
-	void FixedUpdate(){
-		bombSpawn = transform.position;
-		bombSpawn += Vector3.up * 0.35f;
-		
-		if(Input.GetKeyDown(KeyCode.F) && Time.time > tempTime){
-			//Debug.Log(Time.time);
-			tempTime += bombWaitTime;
-			GameObject spawnedBomb = (GameObject)Instantiate(bomb, bombSpawn, transform.rotation);
-			
-			Destroy(spawnedBomb,3);
-		}
 
 	}
+
+
+	
 }
