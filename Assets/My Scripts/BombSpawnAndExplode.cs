145afed2a62cb17cd607bfc3921320b8843f4b5f
Sachin Jain <sachinjain3677@gmail.com>
2017-12-03 17:30:01 +0530

    Scaling Changes

---
+++ Assets/My Scripts/BombSpawnAndExplode.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BombSpawnAndExplode : MonoBehaviour {
+
+	Vector3 bombSpawn;
+	float lastSpawnTime;
+	float nextSpawnTime;
+	GameController gc;
+	
+	public GameObject bomb;
+	public float bombWaitTime;
+	public GameObject player;
+	public GameObject explosion;
+	public int explosionSpread;
+	public float explosionWaitTime;
+	public float explosionStay;
+
+	// Use this for initialization
+	void Start(){
+		lastSpawnTime=nextSpawnTime=0.0f;
+		gc = GameObject.Find("GameController").GetComponent<GameController>();
+	}
+	
+	void FixedUpdate(){
+		bombSpawn = new Vector3(Mathf.Round(player.transform.position.x), Mathf.Round(player.transform.position.y), Mathf.Round(player.transform.position.z));
+		//bombSpawn += Vector3.up * 0.35f;
+		
+		if(Input.GetKeyDown(KeyCode.F) && Time.time > nextSpawnTime){
+			//Debug.Log(Time.time);
+			lastSpawnTime = Time.time;
+			nextSpawnTime = lastSpawnTime + bombWaitTime;
+			GameObject spawnedBomb = (GameObject)Instantiate(bomb, bombSpawn, player.transform.rotation);
+			Destroy(spawnedBomb,3);
+			StartCoroutine(explosionSpawn(bombSpawn, player.transform.rotation));	
+			
+		}
+
+	}
+
+	IEnumerator explosionSpawn(Vector3 bombSpawnCenter, Quaternion rotation){
+		yield return new WaitForSeconds(3.0f);
+
+		GameObject explosionCenter = (GameObject)Instantiate(explosion, bombSpawnCenter, rotation);
+		Destroy(explosionCenter, explosionStay);
+		yield return new WaitForSeconds(explosionWaitTime);
+
+		for(int i=1; i<explosionSpread; i++){
+			
+			spawnExplosion(bombSpawnCenter + Vector3.forward *i, rotation);
+
+			spawnExplosion(bombSpawnCenter + Vector3.back *i, rotation);
+
+			spawnExplosion(bombSpawnCenter + Vector3.left *i, rotation);
+
+			spawnExplosion(bombSpawnCenter + Vector3.right *i, rotation);
+
+
+			// GameObject explosion1 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.forward *i, rotation);
+			// GameObject explosion2 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.back *i, rotation);
+			// GameObject explosion3 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.left*i, rotation);
+			// GameObject explosion4 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.right *i, rotation);
+			// DestroyExplosionObjects(explosion1, explosion2, explosion3, explosion4);
+			yield return new WaitForSeconds(explosionWaitTime);
+		}
+	}
+
+	void spawnExplosion(Vector3 position, Quaternion rotation){
+		if(gc.level[(int)position.x, (int)position.z]==null){
+			GameObject spawnedExplosion = (GameObject)Instantiate(explosion, position, rotation);
+			Destroy(spawnedExplosion, explosionStay);
+		}
+		
+	}
+
+	// void DestroyExplosionObjects(GameObject explosionObject1, GameObject explosionObject2, GameObject explosionObject3, GameObject explosionObject4){
+	// 	Destroy(explosionObject1, explosionStay);
+	// 	Destroy(explosionObject2, explosionStay);
+	// 	Destroy(explosionObject3, explosionStay);
+	// 	Destroy(explosionObject4, explosionStay);
+	// }
+
+}
--- Assets/My Scripts/GameController.cs
+++ Assets/My Scripts/GameController.cs
@@ -2,64 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class GameController : MonoBehaviour {
 
-	Vector3 bombSpawn;
-	float lastSpawnTime;
-	float nextSpawnTime;
-	
-	public GameObject bomb;
-	public float bombWaitTime;
-	public GameObject player;
-	public GameObject explosion;
-	public int explosionSpread;
-	public float explosionWaitTime;
-	public float explosionStay;
+	public GameObject levelHolder;
+	public GameObject[,] level = new GameObject[10, 10];
 
 	// Use this for initialization
-	void Start(){
-		lastSpawnTime=nextSpawnTime=0.0f;
+	void Start () {
+		var objects = levelHolder.GetComponentsInChildren<Transform>();
+	
+		foreach(var child in objects){
+			if(child.gameObject.name!="Level"){
+				level[(int)child.position.x, (int)child.position.z] = child.gameObject;
+			}
+		}
+
+		foreach(var child in level){
+			Debug.Log(child);
+		}
+
+
+		foreach(var child in level){
+			if(child!=null){
+				Debug.Log("["+child.gameObject.transform.position.x+","+child.gameObject.transform.position.y+","+child.gameObject.transform.position.z+"]");
+			}
+		}
+
 	}
 	
-	void FixedUpdate(){
-		bombSpawn = new Vector3(Mathf.Round(player.transform.position.x), Mathf.Round(player.transform.position.y), Mathf.Round(player.transform.position.z));
-		//bombSpawn += Vector3.up * 0.35f;
-		
-		if(Input.GetKeyDown(KeyCode.F) && Time.time > nextSpawnTime){
-			//Debug.Log(Time.time);
-			lastSpawnTime = Time.time;
-			nextSpawnTime = lastSpawnTime + bombWaitTime;
-			GameObject spawnedBomb = (GameObject)Instantiate(bomb, bombSpawn, player.transform.rotation);
-			Destroy(spawnedBomb,3);
-			StartCoroutine(explosionSpawn(bombSpawn, player.transform.rotation));	
+	// Update is called once per frame
+	void test () {
 			
-		}
-
 	}
-
-	IEnumerator explosionSpawn(Vector3 bombSpawnCenter, Quaternion rotation){
-		yield return new WaitForSeconds(3.0f);
-
-		GameObject explosionCenter = (GameObject)Instantiate(explosion, bombSpawnCenter, rotation);
-		Destroy(explosionCenter, explosionStay);
-		yield return new WaitForSeconds(explosionWaitTime);
-
-		for(int i=1; i<explosionSpread; i++){
-			GameObject explosion1 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.forward *i, rotation);
-			GameObject explosion2 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.back *i, rotation);
-			GameObject explosion3 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.left*i, rotation);
-			GameObject explosion4 = (GameObject)Instantiate(explosion, bombSpawnCenter + Vector3.right *i, rotation);
-			DestroyExplosionObjects(explosion1, explosion2, explosion3, explosion4);
-			yield return new WaitForSeconds(explosionWaitTime);
-		}
-	}
-
-	void DestroyExplosionObjects(GameObject explosionObject1, GameObject explosionObject2, GameObject explosionObject3, GameObject explosionObject4){
-		Destroy(explosionObject1, explosionStay);
-		Destroy(explosionObject2, explosionStay);
-		Destroy(explosionObject3, explosionStay);
-		Destroy(explosionObject4, explosionStay);
-	}
-
 }
