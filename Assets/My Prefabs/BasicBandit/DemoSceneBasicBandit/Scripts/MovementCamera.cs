d8972c321e149b083ea9f07c5855d69c75a67a73
chiggywiggy <chiragsharma22021998@gmail.com>
2017-12-26 21:34:42 +0530

    player character and enemies added

---
+++ Assets/My Prefabs/BasicBandit/DemoSceneBasicBandit/Scripts/MovementCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementCamera : MonoBehaviour {
+
+	public GameObject target;
+	public float speedMove = 1.0f;
+
+	Vector3 point;
+
+	void Start () {
+		point = target.transform.position;
+//		transform.LookAt(point);
+	}
+
+	void Update () {
+		transform.RotateAround (point,new Vector3(0.0f,1.0f,0.0f),20 * Time.deltaTime * speedMove);
+	}
+}
