57ef8e52fed7d060466f9fad7fda6223f732447f
sachinjain3677 <sachinjain3677@gmail.com>
2018-01-27 08:03:46 +0530

    buttons working, can spawn only one bomb at once

--- Assets/My Scripts/fire_button_control.cs
+++ Assets/My Scripts/fire_button_control.cs
@@ -6,20 +6,25 @@ public class fire_button_control : MonoBehaviour {
 
 	BombSpawnAndExplode bsae;
 
+	RectTransform button;
+
 	// Use this for initialization
 	void Start () {
 		bsae = GameObject.Find ("GameController").GetComponent<BombSpawnAndExplode> ();
 		bsae.fire_button_pressed = false;
+		button = GetComponent<RectTransform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			bsae.fire_button_pressed = true;
-		}
+		if (Input.mousePosition.y >= button.position.y - button.rect.height / 2 && Input.mousePosition.y <= button.position.y + button.rect.height / 2 && Input.mousePosition.x <= button.position.x + button.rect.width / 2 && Input.mousePosition.x >= button.position.x - button.rect.width / 2) {
+			if (Input.GetMouseButtonDown (0)) {
+				bsae.fire_button_pressed = true;
+			}
 
-		if (Input.GetMouseButtonUp (0)) {
-			bsae.fire_button_pressed = false;
+			if (Input.GetMouseButtonUp (0)) {
+				bsae.fire_button_pressed = false;
+			}
 		}
 	}
 }
--- Assets/My Scripts/left_button_control.cs
+++ Assets/My Scripts/left_button_control.cs
@@ -8,23 +8,30 @@ public class left_button_control : MonoBehaviour {
 
 	PlayerController pc;
 
+	RectTransform button;
+
 	// Use this for initialization
 	void Start () {
 		pc = GameObject.Find("PlayerGameObject").GetComponent<PlayerController>();
-
-		animator = GetComponent<Animator> ();
+		button = GetComponent<RectTransform> ();
+		animator = GameObject.Find("Basic_BanditPrefab Bighead").GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			pc.direction_x = -1;
-			animator.SetBool ("Walk", true);
-		}	
+		if (Input.mousePosition.y >= button.position.y - button.rect.height / 2 && Input.mousePosition.y <= button.position.y + button.rect.height / 2 && Input.mousePosition.x <= button.position.x + button.rect.width / 2 && Input.mousePosition.x >= button.position.x - button.rect.width / 2) {
 
-		if (Input.GetMouseButtonUp (0)) {
-			pc.direction_x = 0;
-			animator.SetBool ("Walk", false);
+			if (Input.GetMouseButtonDown (0)) {
+				pc.direction_x = -1;
+				animator.SetBool ("Walk", true);
+			}	
+
+			if (Input.GetMouseButtonUp (0)) {
+				pc.direction_x = 0;
+				animator.SetBool ("Walk", false);
+			}
 		}
+
+
 	}
 }
