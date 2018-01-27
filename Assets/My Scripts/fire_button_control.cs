07e9bd5e097e6a710125b2d401b3e09c8bf7e879
chiggywiggy <chiragsharma22021998@gmail.com>
2018-01-27 06:20:21 +0530

    Scaled Buttons

---
+++ Assets/My Scripts/fire_button_control.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fire_button_control : MonoBehaviour {
+
+	BombSpawnAndExplode bsae;
+
+	// Use this for initialization
+	void Start () {
+		bsae = GameObject.Find ("GameController").GetComponent<BombSpawnAndExplode> ();
+		bsae.fire_button_pressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			bsae.fire_button_pressed = true;
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			bsae.fire_button_pressed = false;
+		}
+	}
+}
