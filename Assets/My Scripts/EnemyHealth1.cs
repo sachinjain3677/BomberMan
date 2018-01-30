81b2d2cd6fe7d49d9266b1961f4000245ba96dbe
chiggywiggy <chiragsharma22021998@gmail.com>
2018-01-31 04:57:50 +0530

    Enemy Dying completed

---
+++ Assets/My Scripts/EnemyHealth1.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class EnemyHealth1 : MonoBehaviour
+{
+    
+    public float sinkSpeed = 2.5f;
+
+
+    Animator anim;
+    AudioSource enemyAudio;
+    ParticleSystem hitParticles;
+    CapsuleCollider capsuleCollider;
+    bool isDead;
+    bool isSinking;
+
+
+    void Awake ()
+    {
+        anim = GetComponent <Animator> ();
+        enemyAudio = GetComponent <AudioSource> ();
+        capsuleCollider = GetComponent <CapsuleCollider> ();
+		Death ();
+    }
+
+
+    void Update ()
+    {
+        if(isSinking)
+        {
+            transform.Translate (-Vector3.up * sinkSpeed * Time.deltaTime);
+        }
+    }
+
+
+    public void TakeDamage (int amount, Vector3 hitPoint)
+    {
+        if(isDead)
+            return;
+
+        enemyAudio.Play ();
+            
+        
+            Death ();
+    }
+
+
+    void Death ()
+    {
+        isDead = true;
+
+        capsuleCollider.isTrigger = true;
+
+        anim.SetTrigger ("Dead");
+        enemyAudio.Play ();
+    }
+
+
+    public void StartSinking ()
+    {
+        GetComponent <UnityEngine.AI.NavMeshAgent> ().enabled = false;
+        GetComponent <Rigidbody> ().isKinematic = true;
+        isSinking = true;
+        //ScoreManager.score += scoreValue;
+        Destroy (gameObject, 2f);
+    }
+}
