7befaf81feb417303c0501c129cb8a846b9ebd15
sachinjain3677 <sachinjain3677@gmail.com>
2018-02-02 05:24:19 +0530

    fixed game over menu

--- Assets/game_Over.cs
+++ Assets/game_Over.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class game_Over : MonoBehaviour {
 
+	BombSpawnAndExplode bsae;
+
+	public GameObject game_over_menu;
 	// Use this for initialization
 	void Start () {
-		gameObject.SetActive (false);
+		bsae = GameObject.Find ("GameController").GetComponent<BombSpawnAndExplode> ();
+		game_over_menu.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (bsae.isDead) {
+			game_over_menu.SetActive (true);
+		}
 	}
 }
